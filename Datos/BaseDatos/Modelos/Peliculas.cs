485be368b851543dfb6b5b8651545f79c98fddf6
Darling Sierra <darlinghernandez06307@hotmail.com>
2024-03-24 19:20:08 -0600

    Agregar archivos de proyecto.

---
+++ Datos/BaseDatos/Modelos/Peliculas.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.BaseDatos.Modelos
+{
+    public class Peliculas
+    {
+        [Key]
+        [Required]
+        public int PeliculaId { get; set; }
+
+        [Required]
+        [MaxLength(75)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Genero { get; set; }
+
+        [Required]
+        [MaxLength(75)]
+        public string Autores { get; set;}
+
+        [Required]
+        public int Existencia { get; set; }
+
+        [Required]
+        public decimal PrecioRenta { get; set; }
+
+        [Required]
+        public bool Estado { get; set; }
+    }
+}
---
+++ Datos/BaseDatos/RentaVideoContext.cs
@@ -0,0 +1,29 @@
+﻿using Datos.BaseDatos.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.BaseDatos
+{
+    public class RentaVideoContext : DbContext
+    {
+        public RentaVideoContext(): base("RentaVideo1") 
+        {
+            
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public DbSet<Peliculas> peliculas { get; set; }
+    }
+}
